c526a3a7c3981a7a197b066c331fc3a73afbdbd2
Eugenio Kuri <eugenio.kuri@honeywell.com>
2018-06-12 15:02:13 -0500

    chart showing realtime information

--- app/src/main/java/com/honeywell/audioassist/repository/IEventRepository.java
+++ app/src/main/java/com/honeywell/audioassist/repository/IEventRepository.java
@@ -1,9 +1,16 @@
 package com.honeywell.audioassist.repository;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.honeywell.audioassist.model.Event;
+
+import java.util.Date;
 
 public interface IEventRepository {
 
     Query getEventListQuery();
-
+    Query getEventsFromDate(Date startDate);
+    Event parseEvent(DocumentSnapshot doc);
 }
--- app/src/main/java/com/honeywell/audioassist/view/EventStatisticsFragment.java
+++ app/src/main/java/com/honeywell/audioassist/view/EventStatisticsFragment.java
@@ -1,5 +1,7 @@
 package com.honeywell.audioassist.view;
 
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -8,21 +10,60 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.honeywell.audioassist.R;
+import com.honeywell.audioassist.viewmodel.StatisticsViewModel;
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.ValueDependentColor;
 import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 
+import java.util.Map;
+
 public class EventStatisticsFragment extends Fragment {
 
     private GraphView barChart;
+    private StatisticsViewModel mViewModel;
+    private TextView mTodayEventsText;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(StatisticsViewModel.class);
+        mTodayEventsText = view.findViewById(R.id.today_value);
+
+        final Observer<Map<Integer,Integer>> eventObserver = new Observer<Map<Integer, Integer>>() {
+            @Override
+            public void onChanged(@Nullable final Map<Integer, Integer> weeklyEvents) {
+                // Update the UI, in this case, a TextView.
+                setChartValues(weeklyEvents);
+            }
+        };
+
+        final Observer<Integer> todayEventsObserver = new Observer<Integer>() {
+            @Override
+            public void onChanged(@Nullable Integer value) {
+                mTodayEventsText.setText(value.toString());
+            }
+        };
+
+        mViewModel.getmTodaysEvents().observe(this, todayEventsObserver);
+        // Observe the LiveData, passing in this activity as the LifecycleOwner and the observer.
+        mViewModel.getmWeeklyEvents().observe(this, eventObserver);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mViewModel.startListening();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mViewModel.stopListening();
     }
 
     @Nullable
@@ -30,21 +71,6 @@ public class EventStatisticsFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.statistics_fragment, container, false);
         barChart = view.findViewById(R.id.graph);
-        BarGraphSeries<DataPoint> series = new BarGraphSeries<>(new DataPoint[]{
-                new DataPoint(0, 12),
-                new DataPoint(1, 9),
-                new DataPoint(2, 5),
-                new DataPoint(3, 2),
-                new DataPoint(4, 1)
-        });
-        barChart.addSeries(series);
-        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
-            @Override
-            public int get(DataPoint data) {
-                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
-            }
-        });
-        series.setSpacing(20);
 
         barChart.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
             @Override
@@ -60,5 +86,23 @@ public class EventStatisticsFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void setChartValues(Map<Integer, Integer> weeklyEvents){
+           DataPoint[] points = new DataPoint[weeklyEvents.size()];
+           for(Integer key : weeklyEvents.keySet()){
+                points[key] = new DataPoint(key, weeklyEvents.get(key));
+           }
+           barChart.removeAllSeries();
+           BarGraphSeries<DataPoint> series = new BarGraphSeries<>(points);
+        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+            }
+        });
+        series.setSpacing(5);
+        series.setDrawValuesOnTop(true);
+        barChart.addSeries(series);
     }
 }
